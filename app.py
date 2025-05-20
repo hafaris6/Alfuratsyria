fe9407472b1bc774f682c18a53b0852740b99fae
hafaris6 <aldyryalkhal150@gmail.com>
2025-05-20 18:13:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from werkzeug.utils import secure_filename
+from gunicorn app:app import secure_filename
 import os
 
 app = Flask(__name__)
